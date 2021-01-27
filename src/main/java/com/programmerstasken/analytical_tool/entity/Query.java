c15fd389c7164718898acba3da2d06c88a0dc998
sviat-h <svyatoslav.grinik@gmail.com>
2021-01-27 14:23:12 +0200

    changed Query class structure

--- src/main/java/com/programmerstasken/analytical_tool/entity/Query.java
+++ src/main/java/com/programmerstasken/analytical_tool/entity/Query.java
@@ -4,33 +4,87 @@ import java.time.LocalDate;
 
 public class Query {
 
-    private String serviceNumber;
-    private String questionType;
+    private int serviceNumber;
+    private int serviceVariation;
+
+    private int questionType;
+    private int questionCategory;
+    private int questionSubcategory;
+
     private String responseType;
     private LocalDate dateFrom;
     private LocalDate dateTo;
 
-    public Query(String serviceNumber, String questionType, String responseType, LocalDate dateFrom, LocalDate dateTo) {
-        this.serviceNumber = serviceNumber;
-        this.questionType = questionType;
-        this.responseType = responseType;
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
+    public static class Builder {
+        private Query newQuery;
+
+        public Builder() {
+            newQuery = new Query();
+        }
+
+        public Builder withServiceNumber(int serviceNumber) {
+            newQuery.serviceNumber = serviceNumber;
+            return this;
+        }
+
+        public Builder withServiceVariation(int serviceVariation) {
+            newQuery.serviceVariation = serviceVariation;
+            return this;
+        }
+
+        public Builder withQuestionType(int questionType) {
+            newQuery.questionType = questionType;
+            return this;
+        }
+
+        public Builder withQuestionCategory(int questionCategory) {
+            newQuery.questionCategory = questionCategory;
+            return this;
+        }
+
+        public Builder withQuestionSubcategory(int questionSubcategory) {
+            newQuery.questionSubcategory = questionSubcategory;
+            return this;
+        }
+
+        public Builder withResponseType(String responseType) {
+            newQuery.responseType = responseType;
+            return this;
+        }
+
+        public Builder withDateFrom(LocalDate dateFrom) {
+            newQuery.dateFrom = dateFrom;
+            return this;
+        }
+
+        public Builder withDateTo(LocalDate dateTo) {
+            newQuery.dateTo = dateTo;
+            return this;
+        }
+
+        public Query build() {
+            return newQuery;
+        }
     }
 
-    public Query(String serviceNumber, String questionType, String responseType, LocalDate dateFrom) {
-        this.serviceNumber = serviceNumber;
-        this.questionType = questionType;
-        this.responseType = responseType;
-        this.dateFrom = dateFrom;
-    }
-
-    public String getServiceNumber() {
+    public int getServiceNumber() {
         return serviceNumber;
     }
 
-    public String getQuestionType() {
+    public int getServiceVariation() {
+        return serviceVariation;
+    }
+
+    public int getQuestionType() {
         return questionType;
+    }
+
+    public int getQuestionCategory() {
+        return questionCategory;
+    }
+
+    public int getQuestionSubcategory() {
+        return questionSubcategory;
     }
 
     public String getResponseType() {
