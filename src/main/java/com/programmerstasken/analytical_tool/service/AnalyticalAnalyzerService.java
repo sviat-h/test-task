f3beea4afb1fb7d5b2688f9209eddc17d64c2c21
sviat-h <svyatoslav.grinik@gmail.com>
2021-01-28 15:43:29 +0200

    added required classes (not yet implemented)

---
+++ src/main/java/com/programmerstasken/analytical_tool/service/AnalyticalAnalyzerService.java
@@ -0,0 +1,5 @@
+package com.programmerstasken.analytical_tool.service;
+
+public class AnalyticalAnalyzerService {
+    //TODO fill the class
+}
---
+++ src/main/java/com/programmerstasken/analytical_tool/utils/QueryValidator.java
@@ -0,0 +1,5 @@
+package com.programmerstasken.analytical_tool.utils;
+
+public class QueryValidator {
+    //TODO fill the class
+}
